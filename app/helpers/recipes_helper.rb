a311844f7207295e1a5a52240de11ecce0694e22
Christian Santos <christiangraca@hotmail.com>
2016-12-27 04:15:54 -0200

    utilizando helpers

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,11 @@
 module RecipesHelper
+
+ def format_calories(recipe)
+        if recipe.light?
+          content_tag(:strong, 'Light (contem menos de 100 calorias)')
+        else
+          recipe.calories
+        end
+ end
+
 end
